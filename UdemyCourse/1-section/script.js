0a4253ad7446d437b02fce81c9c03562e6120959
KrzysztofKrych <35628303+KrzysztofKrych@users.noreply.github.com>
2018-03-13 09:40:50 +0100

    Add files via upload

---
+++ UdemyCourse/1-section/script.js
@@ -0,0 +1,14 @@
+document.write('Welcome');
+alert('Hello');
+console.log('It worked');
+
+
+/////////CONSOLE//////////
+//console.log(4+6);
+//
+//console.error("error");
+//
+//console.warn('warning');
+//
+//console.log('%cHello World',"color:green");
+//console.log('%cHello World',"background:black;color:yellow");
