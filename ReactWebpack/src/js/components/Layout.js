4a686a77c8b450740e8c857ce9f8fea84032de7c
KrzysztofKrych <35628303+KrzysztofKrych@users.noreply.github.com>
2018-03-13 09:34:36 +0100

    Add files via upload

---
+++ ReactWebpack/src/js/components/Layout.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import Header from './Header.js'
+import Footer from './Footer.js'
+
+
+
+export default class Layout extends React.Component{
+constructor(){
+    super();
+         this.state = {
+          title: 'Welcome',
+        };
+}
+    
+changeTitle(title){
+     this.setState({title});
+}
+    render(){
+        return(
+
+        <div>
+          <Header changeTitle={this.changeTitle.bind(this)} title={this.state.title}/>
+          
+          <Footer />
+        </div>
+       
+        );
+    }
+}
